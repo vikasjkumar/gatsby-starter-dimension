8cceed373eef7ccca249256a16f49ad4f1537eba
vikasjkumar <vikasjaswalkumar@outlook.com>
2020-04-27 14:23:11 -0400

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter - Dimension V2',
-    author: 'Hunter Chang',
-    description: 'A Gatsby.js V2 Starter based on Dimension by HTML5 UP',
+    title: 'Portfolio',
+    author: 'Vikas Kumar',
+    description: 'Website to host my personal work portfolio as a junior strucutral engineer.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
