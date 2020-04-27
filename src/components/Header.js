81cb1a18c3854b6375b3ead7999393881483aded
vikasjkumar <vikasjaswalkumar@outlook.com>
2020-04-27 14:31:53 -0400

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ const Header = props => (
       <div className="inner">
         <h1>Vikas Kumar</h1>
         <p>
-          Junior structural engineer with a strong interest in structural engineering, sustainable design, architecture and urban studies.{' '}
+          Portfolio of a junior structural engineer with a strong interest in structural engineering, sustainable design, architecture and urban studies.{' '}
           <a href="https://html5up.net">HTML5 UP</a> and released
           <br />
           for free under the{' '}
