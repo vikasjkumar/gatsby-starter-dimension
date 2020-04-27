fb124686079d8b4d459bcba64ed20e7f6917dad2
vikasjkumar <vikasjaswalkumar@outlook.com>
2020-04-27 15:07:15 -0400

    update

--- src/components/Main.js
+++ src/components/Main.js
@@ -147,18 +147,9 @@ class Main extends React.Component {
               </a>
             </li>
             <li>
-              <a href="https://codebushi.com" className="icon fa-facebook">
-                <span className="label">Facebook</span>
-              </a>
-            </li>
-            <li>
-              <a href="https://codebushi.com" className="icon fa-instagram">
-                <span className="label">Instagram</span>
-              </a>
-            </li>
             <li>
               <a
-                href="https://github.com/codebushi/gatsby-starter-dimension"
+                href="https://github.com/vikasjkumar"
                 className="icon fa-github"
               >
                 <span className="label">GitHub</span>
